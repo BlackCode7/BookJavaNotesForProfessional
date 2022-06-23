be07bb85a6fd1ae046d1620e529b9acd592f6f99
Anderson <BlackCode7@outlook.com>
2022-06-23 07:44:13 -0300

    If string using ',', separates for where it has punctuation

--- src/Chapter14StringTokenizer/Chapter14StringTokenizer.java
+++ src/Chapter14StringTokenizer/Chapter14StringTokenizer.java
@@ -4,16 +4,18 @@ import java.util.StringTokenizer;
 
 public class Chapter14StringTokenizer {
 	
-	static String  stringToken = "The set of delimiters (the characters that separate tokens) may be specified "
-			+ "either at creation time or on a pertoken basis.";
+	static String  stringToken = "The set of delimiters, (the characters that separate tokens) may be specified "
+			+ "either, at creation time or on a pertoken basis.";
 
 	public static void main(String[] args) {
 		useCaseStringTokenizer(stringToken);		
 	}
 
 	// Function for tokenizer strings
+	// If string using ',', separates for where it has punctuation	
 	public static void useCaseStringTokenizer( String token ) {
-		StringTokenizer stringTokenizer = new StringTokenizer(stringToken);
+		//StringTokenizer stringTokenizer = new StringTokenizer(stringToken, " ");
+		StringTokenizer stringTokenizer = new StringTokenizer(stringToken, ",");
 		while( stringTokenizer.hasMoreTokens() ) {
 			System.out.println(stringTokenizer.nextToken());
 		}
